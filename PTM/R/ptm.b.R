bf99ce8d070d003669aa4e98ba8f1b1adac9e95a
Jeff Walker <walker@maine.edu>
2024-06-22 12:45:57 -0400

    ggplot_the_model updates

--- PTM/R/ptm.b.R
+++ PTM/R/ptm.b.R
@@ -420,250 +420,31 @@ PTMClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
       }
       
       #            table <- self$results$ttest # use this line to find if code gets this far
-      # create the datasets of measurement reps
-      # for non-nested data, these are the experimental reps
-      # for nested data, these are technical reps
-      # then compute the nest means = experimental reps next
-      
+
+
       if(working_model == TRUE){
-        plotData_full <- data.table(
-          dataset = "exp_reps", # change this to technical_reps if nested
-          y = self$data[[self$options$dep]],
-          factor_1 = self$data[[self$options$factor1]]
-        )
-        if(two_factors == FALSE){
-          plotData_full[, factor_2 := factor_1]
-          plotData_full[, plot_factor := factor_1]
-        }else{
-          plotData_full[, factor_2 := self$data[[self$options$factor2]]]
-          plotData_full[, plot_factor := paste(factor_1, factor_2,
-                                               sep = "\n")]
-          # reorder factor levels for 2-factor plots
-          levels_1 <- levels(plotData_full$factor_1) |>
-            as.character()
-          levels_2 <- levels(plotData_full$factor_2) |>
-            as.character()
-          levels_table <- expand.grid(levels_1, levels_2, stringsAsFactors = FALSE) |>
-            data.table()
-          levels_table[, plot_levels := paste(Var1, Var2, sep = "\n")]
-          plotData_full[, plot_factor := factor(plot_factor,
-                                                levels = levels_table[, plot_levels])]
-        }
-        
-        if(include_block == FALSE){
-          plotData_full[, block_id := NA]
-        }else{
-          plotData_full[, block_id := self$data[[self$options$block]]]
-        }
-        if(include_nest == FALSE){
-          plotData_full[, nest_id := NA]
-        }else{
-          plotData_full[, nest_id := self$data[[self$options$nest_id]]]
-        }
-        plotData_full[, mean := as.numeric(NA)]
-        plotData_full[, SE := as.numeric(NA)]
-        plotData_full[, lo := as.numeric(NA)]
-        plotData_full[, hi := as.numeric(NA)]
-        
-        # compute nest means if nested
-        if(include_nest == TRUE){
-          nest_means <- plotData_full[, .(y = mean(y, na.rm = TRUE)),
-                                      by = .(factor_1, factor_2, plot_factor, block_id, nest_id)]
-          # for prediction to get y, we need factor nest and block labels
-          # convert to original column labels
-          new_data <- copy(nest_means)
-          setnames(new_data,
-                   old = c("factor_1", "nest_id"),
-                   new = c(factor1_label, nest_label))
-          if(two_factors == TRUE){
-            setnames(new_data,
-                     old = "factor_2",
-                     new = factor2_label)
-          }
-          if(include_block == TRUE){
-            setnames(new_data,
-                     old = "block_id",
-                     new = block_label)
-          }
-          
-          y_hat <- predict(m1, new_data)
-          if(family_class == "generalized"){
-            y_hat <- predict(m1, new_data, type = "response")
-          }
-          plotData_nest_means <- data.table(
-            dataset = "exp_reps",
-            y = y_hat,
-            factor_1 = nest_means[, factor_1],
-            factor_2 = nest_means[, factor_2],
-            plot_factor = nest_means[, plot_factor],
-            block_id = nest_means[, block_id],
-            nest_id = nest_means[, nest_id],
-            mean = NA,
-            SE = NA,
-            lo = NA,
-            hi = NA
-          )
-          plotData_full[, dataset := "tech_reps"]
-        }
-        
-        # create dataset for m1_check
-        if(include_nest == TRUE){
-          new_data[, y := y_hat]
-          setnames(new_data, old = "y", new = response_label)
-          #              data_check <- copy(new_data)
-        }
-        
-        # m1_emm and m1_pairs have variable columns and column names so here I fit
-        # and 2X data flattened (all combos in a single treatment factor) using 
-        # lm (even if the specified model is mixed or generalized). This is because
-        # I want the x-axis labels to be those of the flattened model. So all of
-        # this just get's me the plot_factor column - the means, CIs, p-values
-        # are all from the specified model fit.
-        
-        # these are the tables needed to send to ggplot
-        m1_emm_dt <- m1_emm |>
-          summary() |>
-          data.table()
-        m1_pairs_dt <- m1_pairs |>
-          data.table()
-        # these are the tables that get the plot_factor column correct
-        m1_plot <- lm(y ~ plot_factor, plotData_full)
-        emm_plot <- emmeans(m1_plot, specs = "plot_factor")
-        pairs_plot <- contrast(emm_plot,
-                               method = "revpairwise",
-                               adjust = "none") |>
-          summary(infer = TRUE) |>
-          data.table()
-        if(two_factors == FALSE){
-          plot_factor_names <- summary(emm_plot)[,1] # aov_4 can modify factor names so this returns it to original
-          colnames(m1_emm_dt) <- c("plot_factor", "mean", "SE", "df", "lo", "hi")
-          m1_emm_dt[, plot_factor := plot_factor_names]
-          m1_pairs_dt[, contrast := pairs_plot[, 1]]
-        }else{
-          if(plot_simple == TRUE){
-            # keep simple effects only
-            levels_1 <- levels(plotData_full$factor_1) |>
-              as.character()
-            levels_2 <- levels(plotData_full$factor_2) |>
-              as.character()
-            p1 <- length(levels_1)
-            p2 <- length(levels_2)
-            pp <- p1*p2
-            rows_list <- numeric(pp)
-            j <- 0
-            for(i in 1:(pp/2)){
-              j <- j + 1
-              a <- i
-              b <- (pp * (pp - 1))/2 - i + 1
-              rows_list[j] <- a
-              j <- j + 1
-              rows_list[j] <- b
-            }
-            pairs_plot <- pairs_plot[rows_list,]
-          }
-          new_contrast_col <- pairs_plot[, contrast]
-          m1_pairs_dt[, contrast := new_contrast_col]
-          
-          # replace flattened names in m1_emm
-          colnames(m1_emm_dt) <- c("factor1", "factor2", "mean", "SE", "df", "lo", "hi")
-          m1_emm_dt[, plot_factor := paste(factor1, factor2, sep = "\n")]
-          m1_emm_dt <- m1_emm_dt[, .SD, .SDcols = c("plot_factor", "mean", "SE", "df", "lo", "hi")]
-        }
-        # unify colnames for different kinds of emm and pairs tables
-        # colnames(emm_plot)[2,5,6] <- c("mean", "lo", "hi")
-        setnames(m1_pairs_dt, old = "SE", new = "SED")
-        
-        
-        # fill out summary table for means and error bars
-        y_cols <- c("dataset", "y", "factor_1", "factor_2", "plot_factor",
-                    "block_id", "nest_id", "mean", "SE", "lo", "hi")
-        plotData_summary <- data.table(
-          dataset = "emmeans",
-          y = as.numeric(NA),
-          factor_1 = NA,
-          factor_2 = NA,
-          block_id = NA,
-          nest_id = NA,
-          m1_emm_dt
-        )
-        plotData_summary <- plotData_summary[, .SD, .SDcols = y_cols]
-        
-        # combine summary and full data because ggplot in jmv can
-        # only take a single dataset
-        plotData <- rbind(plotData_summary, plotData_full)
-        if(include_nest){
-          plotData <- rbind(plotData, plotData_nest_means)
-        }
-        
-        # for plot, I need integer for xpos of groups since
-        # jmv isn't correctly ordering factor levels
-        plotData[, plot_factor_id := as.integer(plot_factor) |>
-                   as.character()]
-        plot_factor_levels <- levels(plotData$plot_factor)
-        
-        # p-value table
-        groups <- unlist(str_split(m1_pairs_dt$contrast, " - "))
-        # remove parentheses if they exist from group cols
-        groups <- lapply(groups, remove_parentheses) |>
-          unlist()
-        i_seq <- 1:length(groups)
-        m1_pairs_dt[, group1 := groups[i_seq%%2 != 0] |>
-                      factor(levels = levels(plotData_full$plot_factor))]
-        m1_pairs_dt[, group2 := groups[i_seq%%2 == 0] |>
-                      factor(levels = levels(plotData_full$plot_factor))]
-        m1_pairs_dt[, minx := as.integer(group1) |> as.character()]
-        m1_pairs_dt[, maxx := as.integer(group2) |> as.character()]
-        m1_pairs_dt[, p.print := ifelse(p.value > 0.1,
-                                        p_format(round(p.value, digits = 2),
-                                                 add.p = TRUE, leading.zero = FALSE, space = FALSE),
-                                        p_format(round(p.value, digits = 3),
-                                                 add.p = TRUE, leading.zero = FALSE, space = FALSE))]
-        # get y position of p-value brackets
-        if(show_tech_reps == TRUE){
-          maxy <- max(plotData[dataset == "tech_reps", y], na.rm = TRUE)
-          miny <- min(plotData[dataset == "tech_reps", y], na.rm = TRUE)
-        }else{
-          maxy <- max(plotData[dataset == "exp_reps", y], na.rm = TRUE)
-          miny <- min(plotData[dataset == "exp_reps", y], na.rm = TRUE)
-        }
-        p_increment <- 0.05*(maxy - miny)
-        m1_pairs_dt[, y_pos := maxy + .I * p_increment ]
-        
-        # add pairs_plot to plotData
-        n_rows <- nrow(plotData) - nrow(pairs_plot)
-        n_col <- ncol(m1_pairs_dt)
-        add_na <- matrix(nrow = n_rows, ncol = n_col)
-        colnames(add_na) <- colnames(m1_pairs_dt)
-        m1_pairs_dt <- rbind(m1_pairs_dt, add_na)
-        
-        plotData <- cbind(plotData, m1_pairs_dt)
+        image <- self$results$plot
+        # image$setState(plotData)
+        model_list <- list(m1, m1_emm, m1_pairs)
+        image$setState(model_list)
       }
-      if(working_model == FALSE){
-        plotData <- NULL
-      }
-      #     if(working_model == TRUE){
-      image <- self$results$plot
-      # image$setState(plotData)
-      model_list <- list(m1, m1_emm)
-      image$setState(model_list)
-      #      }
       
       # prepare m1_check model for check_the_model
       # use nest means if there are subsampled reps
       m1_check <- NULL
-      if(working_model == TRUE){
-        if(check_the_model == TRUE){
-          m1_check <- copy(m1)
-        }else{
-          m1_check <- NULL
-        }
-        if(model_class == "anova"){m1_check <- NULL}
-        do_model_check <- TRUE
-        if(do_model_check == TRUE){
-          image_check <- self$results$check
-          image_check$setState(m1_check)
-        }
-      }
+      # if(working_model == TRUE){
+      #   if(check_the_model == TRUE){
+      #     m1_check <- copy(m1)
+      #   }else{
+      #     m1_check <- NULL
+      #   }
+      #   if(model_class == "anova"){m1_check <- NULL}
+      #   do_model_check <- TRUE
+      #   if(do_model_check == TRUE){
+      #     image_check <- self$results$check
+      #     image_check$setState(m1_check)
+      #   }
+      # }
       
       ### DEBUG ONLY remove this when 
       m1_check <- NULL
@@ -673,7 +454,20 @@ PTMClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
       model_list <- image$state
       m1 <- model_list[[1]]
       m1_emm <- model_list[[2]]
-      gg <- plot_response(m1, m1_emm)
+      m1_pairs <- model_list[[3]]
+      
+      # plot features
+      show_nest_data_val <- ifelse(self$options$tech_reps == TRUE, TRUE, FALSE)
+      nest_id_val <- ifelse(is.null(self$options$nest_id), NA, self$options$nest_id)
+      palette_val <- self$options$pal
+
+      # get the plot!
+      gg <- plot_response(m1,
+                          m1_emm,
+                          m1_pairs,
+                          show_nest_data = show_nest_data_val,
+                          nest_id = nest_id_val,
+                          palette = palette_val)
       print(gg)
       TRUE
     },
